31ba7ff472906cfcc059487df8b00e4f35263082
Jake Jacobson <Jake Jacobson@JCON-Laptop>
2017-03-26 15:15:27 -0500

    setset

--- AVLServer/src/tester.java
+++ AVLServer/src/tester.java
@@ -4,6 +4,7 @@ public class tester {
 	public static void main(String args[]){
 		Vehicle newVeh = new Vehicle(5,'A',35.8,-95.6,50.0,120.0);
 		System.out.println(newVeh.toString());
+		//THIS IS COMMENT
 		
 	}
 }
